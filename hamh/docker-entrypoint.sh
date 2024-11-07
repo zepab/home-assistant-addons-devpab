7a8c73c2adf15dd2c8822796fd589f22a505f87a
t0bst4r <t0bst4r@outlook.com>
2024-11-07 15:09:31 +0100

    bumb(home-assistant-matter-hub): version 3.0.0-alpha.20

--- hamh/docker-entrypoint.sh
+++ hamh/docker-entrypoint.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/with-contenv bashio
 
 home-assistant-matter-hub start \
-  --log-level=$(bashio::config 'log_level') \
+  --log-level=$(bashio::config 'app_log_level') \
   --disable-log-colors=$(bashio::config 'disable_log_colors') \
   --storage-location=/config/data \
   --web-port=$(bashio::config 'web_port') \
