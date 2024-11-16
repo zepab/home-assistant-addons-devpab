431b09835eb877225f34d16f247d004e35509d9b
t0bst4r <t0bst4r@outlook.com>
2024-11-16 18:54:09 +0100

    chore: ingress test with test-addon

--- hamh_test/docker-entrypoint.sh
+++ hamh_test/docker-entrypoint.sh
@@ -4,6 +4,6 @@ home-assistant-matter-hub start \
   --log-level=$(bashio::config 'log_level') \
   --disable-log-colors=$(bashio::config 'disable_log_colors') \
   --storage-location=/config/data \
-  --web-port=8482 \
+  --web-port="$(bashio::addon.ingress_port)" \
   --home-assistant-url='http://supervisor/core' \
   --home-assistant-access-token="$SUPERVISOR_TOKEN"
