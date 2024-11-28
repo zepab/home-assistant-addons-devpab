449a008a6675879bc84d9325edb4cc2923b75065
Tobias Glatthar <t0bst4r@outlook.com>
2024-11-28 20:46:56 +0100

    chore: update test-addon

--- hamh_test/docker-entrypoint.sh
+++ hamh_test/docker-entrypoint.sh
@@ -1,9 +1,14 @@
 #!/usr/bin/with-contenv bashio
 
+bashio::addon.config
+echo ""
+bashio::addon.ingress_port
+echo ""
+
 home-assistant-matter-hub start \
-  --log-level=$(bashio::config 'log_level') \
+  --log-level=$(bashio::config 'app_log_level') \
   --disable-log-colors=$(bashio::config 'disable_log_colors') \
   --storage-location=/config/data \
-  --web-port="$(bashio::addon.ingress_port)" \
+  --web-port=$(bashio::addon.ingress_port) \
   --home-assistant-url='http://supervisor/core' \
   --home-assistant-access-token="$SUPERVISOR_TOKEN"
